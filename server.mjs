d107dd1d5f3211c6e20add54538396badcaf9930
PamelaTerrell <pammyhoney@yahoo.com>
2024-10-18 15:37:53 -0400

    Lab319.4.1

---
+++ server.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import gradeRoutes from './db/routes/gradeRoutes.mjs'
+
+// SetUps
+const app = express()
+dotenv.config()
+let PORT = process.env.PORT || 3001
+
+// middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
+
+// routes
+app.use('/grades', gradeRoutes)
+
+// listener
+app.listen(PORT, ()=>{
+    console.log(`Server Running on Port: ${PORT}`)
+})
+
+
+
